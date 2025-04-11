e3828287f1a404b9981ee8c3d412d9d22a6af0aa
Jackson Coxson <jkcoxson@gmail.com>
2025-04-11 14:37:12 -0600

    Add sourcekit to lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,9 +8,21 @@ return {
 				},
 			},
 		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.sourcekit.setup({})
-		end,
+		-- config = function()
+		-- 	local lspconfig = require("lspconfig")
+		-- 	local keys = require("lazyvim.plugins.lsp.keymaps").get()
+		--
+		-- 	lspconfig.sourcekit.setup({})
+		-- 	keys[#keys + 1] = {
+		-- 		"<leader>cr",
+		-- 		function()
+		-- 			local inc_rename = require("inc_rename")
+		-- 			return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+		-- 		end,
+		-- 		expr = true,
+		-- 		desc = "Rename (inc-rename.nvim)",
+		-- 		has = "rename",
+		-- 	}
+		-- end,
 	},
 }
